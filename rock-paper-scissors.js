d302f0381b8b17737ea34471bf9b3d059934da4d
gorgyboy-dev <161880997+gorgyboy-dev@users.noreply.github.com>
2024-06-06 16:28:03 +0200

    Create getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 100);
+
+    if (num < 33) {
+        return "rock";
+    } else if (num < 66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
