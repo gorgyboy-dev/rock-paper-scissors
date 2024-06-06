0e7eab492e27d554e4fa0accce7a93eedb37d9f4
gorgyboy-dev <161880997+gorgyboy-dev@users.noreply.github.com>
2024-06-06 18:59:09 +0200

    Create playRound function and rewrite getHumanChoice logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,19 +13,89 @@ function getComputerChoice() {
 
 function getHumanChoice() {
 
-    let choice = parseInt(
-        prompt("Press 1 for rock, 2 for paper, and 3 for scissors!")
-    );
+    let choice = prompt("Type your option!").toLowerCase();
 
     switch (choice) {
-        case 1:
+        case "rock":
             return "rock";
-        case 2:
+        case "paper":
             return "paper";
-        case 3:
+        case "scissors":
             return "scissors";
         default:
             alert("Not a valid option!");
             break;
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "paper":
+                    console.log(
+                        `You lose! ${computerChoice} beats ${humanChoice}`);
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log(
+                        `You win! ${humanChoice} beats ${computerChoice}`);
+                    humanScore++;
+                    break;
+                default:
+                    console.log(
+                        `A draw! both have chosen ${humanChoice}`);
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "scissors":
+                    console.log(
+                        `You lose! ${computerChoice} beats ${humanChoice}`);
+                    computerScore++;
+                    break;
+                case "rock":
+                    console.log(
+                        `You win! ${humanChoice} beats ${computerChoice}`);
+                    humanScore++;
+                    break;
+                default:
+                    console.log(
+                        `A draw! both have chosen ${humanChoice}`);
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    console.log(
+                        `You lose! ${computerChoice} beats ${humanChoice}`);
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log(
+                        `You win! ${humanChoice} beats ${computerChoice}`);
+                    humanScore++;
+                    break;
+                default:
+                    console.log(
+                        `A draw! both have chosen ${humanChoice}`);
+                    break;
+            }
+            break;
+        default:
+            alert("Not a valid option!");
+            break;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore + " --- " + computerScore);
