25684339c30aab0fb8d6a972a819360605837e67
gorgyboy-dev <161880997+gorgyboy-dev@users.noreply.github.com>
2024-06-06 16:44:02 +0200

    Create getHumanChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,5 @@
 function getComputerChoice() {
+
     let num = Math.floor(Math.random() * 100);
 
     if (num < 33) {
@@ -7,5 +8,24 @@ function getComputerChoice() {
         return "paper";
     } else {
         return "scissors";
+    }
+}
+
+function getHumanChoice() {
+
+    let choice = parseInt(
+        prompt("Press 1 for rock, 2 for paper, and 3 for scissors!")
+    );
+
+    switch (choice) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            alert("Not a valid option!");
+            break;
     }
 }
